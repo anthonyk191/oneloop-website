30427d1326dfa56d41b6a5cfeec7efa307bc7411
Wanyue Zhai <regina_zhai@yahoo.com>
2021-05-17 20:36:32 -0700

    added timeline

--- src/components/about/about_body.js
+++ src/components/about/about_body.js
@@ -36,20 +36,50 @@ const About_body = () =>{
 				</div>
 				<div className="OurHistory">
 					<h2 className="secTitle"> Our History </h2>
-					<p className="YearNum">2017</p>
-					<p>In 2017-2018 OneLoop applied to SpaceX hyperloop competition. In this year the team was highly ambitious and in preparation for that competition implemented within the pod air bearings, linear induction motor (LIM) propulsion, eddy current brakes, friction brakes, and wheels. OneLoop was able to make it to top 40 through the preliminary design.</p>
+					
 					<div className="Popup2017">
 						<p>It is important to mention that during this time the linear induction motor had not yet been perfected. However the team wanted to test their current version of the LIM as it is a system crucial for energy efficient long distance travel. The industrial air bearings were chosen at the time to allow travel over non ideal surfaces that had small cracks such as shop floors. The bearings can lift up to 1000 lbs making its potential payload particularly effective. </p>
 					</div>
-					<p className="YearNum">2018</p>
-					<p>OneLoop produced a robust pod capable of reaching 100 mph that was designed with safety as a top priority. With the increased level of safety the team implemented in design, OneLoop managed to place 9th out of the 40 teams that entered the SpaceX competition of 2019. </p>
+					
 					<div className="Popup2018">
 						<p>Safety standards such as OSHA and ASME were adhered to with the regards to pod design, testing, and operation whenever relevant. Furthermore, the year’s propulsion system was modified to be cold gas thrusters which was primarily chosen to reduce the pod’s high voltage requirements and increase electrical safety.</p>
 					</div>
-					<p className="YearNum">2019</p>
-					<p>Due to the concerns created by COVID-19, there were no official announcements for SpaceX’s competition. The team switched to a two-year design cycle to adjust to these changes spending more of their time on research and simulations. The acquisition of sponsorship with ANSYS allowed the team to extensively simulate their pod models. With the help of computer aided software, the team was able to drastically improve their mathematical models as well as better ways to integrate the pod. </p>
+					
+
+				<div className="timeline">
+					
+					<img></img>
+					
+  					<div className="container right">
+   						 <div className="content">
+							<p className="YearNum">2017</p>
+							<p>In 2017-2018 OneLoop applied to SpaceX hyperloop competition. In this year the team was highly ambitious and in preparation for that competition implemented within the pod air bearings, linear induction motor (LIM) propulsion, eddy current brakes, friction brakes, and wheels. OneLoop was able to make it to top 40 through the preliminary design.</p>
+    					</div>
+ 					</div>
+
+  					<div className="container left">
+    					<div className="content">
+							<p className="YearNum">2018</p>
+							<p>OneLoop produced a robust pod capable of reaching 100 mph that was designed with safety as a top priority. With the increased level of safety the team implemented in design, OneLoop managed to place 9th out of the 40 teams that entered the SpaceX competition of 2019. </p>
+    					</div>
+  					</div>
+
+  					
+					<img></img>
+  					
+
+  					
+    				<img></img>
+					
+
+  					<div className="container right">
+    					<div className="content">
+							<p className="YearNum">2019</p>
+							<p>Due to the concerns created by COVID-19, there were no official announcements for SpaceX’s competition. The team switched to a two-year design cycle to adjust to these changes spending more of their time on research and simulations. The acquisition of sponsorship with ANSYS allowed the team to extensively simulate their pod models. With the help of computer aided software, the team was able to drastically improve their mathematical models as well as better ways to integrate the pod. </p>
+    					</div>
+  					</div>
 				</div>
-				
+				</div>
 			</div>
 		</React.Fragment>
 	);
