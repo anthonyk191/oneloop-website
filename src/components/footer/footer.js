c438dfb8f298da32fe8e3635f4e7c866694f60f8
Raunak Anand <rsanand@ucdavis.edu>
2021-05-19 23:19:18 +0530

    created navbar and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,14 @@ const Footer = () =>{
 	return(
 		<React.Fragment>
 			<div className="oneloop-footer">
-				<h1>Footer!</h1>
+				<a href="/"><img src="images/OneLoop.svg" /></a>
+				<div className="oneloop-links">
+					<a href="/"><img src="images/linkedin.svg" /></a>
+					<a href="/"><img src="images/twitter.svg" /></a>
+					<a href="/"><img src="images/mail.svg" /></a>
+					<a href="/"><img src="images/instagram.svg" /></a>
+					<a href="/"><img src="images/facebook.svg" /></a>
+				</div>
 			</div>
 		</React.Fragment>
 	);
---
+++ src/components/navbar/RightNav.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Ul = styled.ul`
+	margin-top: 0px;
+  	list-style: none;
+  	display: flex;
+  	flex-flow: row nowrap;
+  	li {
+    	padding: 18px 50px;
+  	}
+    li a {
+      font-size: 16px;
+      color: #fff;
+      text-decoration: none;
+    }
+    li a:hover {
+      color: #fff;
+      text-decoration: underline;
+      text-decoration-color: #fff;
+    }
+  	@media (max-width: 768px) {
+    	flex-flow: column nowrap;
+    	background-color: #022851;
+    	position: fixed;
+    	transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    	top: 0;
+    	right: 0;
+    	height: 100vh;
+    	width: 300px; 
+    	padding-top: 3.5rem;
+    	transition: transform 0.3s ease-in-out;
+    	li {
+      		color: #fff;
+    	}
+  	}
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <Ul open={open}>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About Us</a></li>
+      <li><a href="/teamintro">Teams</a></li>
+      <li><a href="/sponsors">Sponsors</a></li>
+      <li><a href="/poddetails">Pod</a></li>
+      <li><a href="/joinus">Join</a></li>
+    </Ul>
+  )
+}
+
+export default RightNav;
