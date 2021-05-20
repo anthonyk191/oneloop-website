4a1ec8b52befa4e31865327154ac37f739cfdbac
Wanyue Zhai <regina_zhai@yahoo.com>
2021-05-19 18:01:49 -0700

    added clickable

--- src/components/about/about_body.js
+++ src/components/about/about_body.js
@@ -3,12 +3,18 @@ import './about_body.css';
 import React, { useEffect } from 'react';
 
 const About_body = () =>{
+	const [showResults, setShowResults] = React.useState(false)
+  	const onClick = () => setShowResults(true)
+
+	const [showResults1, setShowResults1] = React.useState(false)
+  	const onClick1 = () => setShowResults1(true)
 	return(
 		<React.Fragment>
 			<div className="oneloop-about-body">
 
 				<h1 className="AboutUs">About Us</h1>
 				<p className="imgsub">Team 2018 - 2019</p>
+				<div className="Upper">
 				<div className="OurMission">
 					<h2 className="secTitle"> Our Mission </h2>
 					<li>
@@ -34,16 +40,9 @@ const About_body = () =>{
 						<p>University of California, Davis</p>
 					</div>
 				</div>
+				</div>
 				<div className="OurHistory">
 					<h2 className="secTitle"> Our History </h2>
-					
-					<div className="Popup2017">
-						<p>It is important to mention that during this time the linear induction motor had not yet been perfected. However the team wanted to test their current version of the LIM as it is a system crucial for energy efficient long distance travel. The industrial air bearings were chosen at the time to allow travel over non ideal surfaces that had small cracks such as shop floors. The bearings can lift up to 1000 lbs making its potential payload particularly effective. </p>
-					</div>
-					
-					<div className="Popup2018">
-						<p>Safety standards such as OSHA and ASME were adhered to with the regards to pod design, testing, and operation whenever relevant. Furthermore, the year’s propulsion system was modified to be cold gas thrusters which was primarily chosen to reduce the pod’s high voltage requirements and increase electrical safety.</p>
-					</div>
 					
 
 				<div className="timeline">
@@ -54,14 +53,35 @@ const About_body = () =>{
    						 <div className="content">
 							<p className="YearNum">2017</p>
 							<p>In 2017-2018 OneLoop applied to SpaceX hyperloop competition. In this year the team was highly ambitious and in preparation for that competition implemented within the pod air bearings, linear induction motor (LIM) propulsion, eddy current brakes, friction brakes, and wheels. OneLoop was able to make it to top 40 through the preliminary design.</p>
-    					</div>
+							<p className="collapable">It is important to mention that during this time the linear induction motor had not yet been perfected. However the team wanted to test their current version of the LIM as it is a system crucial for energy efficient long distance travel. The industrial air bearings were chosen at the time to allow travel over non ideal surfaces that had small cracks such as shop floors. The bearings can lift up to 1000 lbs making its potential payload particularly effective. </p>
+							
+						</div>
+						<button onClick={onClick}>click here to learn more</button>
+						{ showResults ? 
+							<div className="Popup2017">
+								<button onClick={()=>setShowResults(false)}>X</button>
+								<p className="Title">2017-2018</p>
+								<p>It is important to mention that during this time the linear induction motor had not yet been perfected. However the team wanted to test their current version of the LIM as it is a system crucial for energy efficient long distance travel. The industrial air bearings were chosen at the time to allow travel over non ideal surfaces that had small cracks such as shop floors. The bearings can lift up to 1000 lbs making its potential payload particularly effective. </p>
+							</div>
+						 : null }
  					</div>
+
 
   					<div className="container left">
     					<div className="content">
 							<p className="YearNum">2018</p>
 							<p>OneLoop produced a robust pod capable of reaching 100 mph that was designed with safety as a top priority. With the increased level of safety the team implemented in design, OneLoop managed to place 9th out of the 40 teams that entered the SpaceX competition of 2019. </p>
-    					</div>
+							<p className="collapable">Safety standards such as OSHA and ASME were adhered to with the regards to pod design, testing, and operation whenever relevant. Furthermore, the year’s propulsion system was modified to be cold gas thrusters which was primarily chosen to reduce the pod’s high voltage requirements and increase electrical safety.</p>
+							
+						</div>
+						<button onClick={onClick1}>click here to learn more</button>
+						{ showResults1 ? 
+							<div className="Popup2018">
+							<button onClick={()=>setShowResults1(false)}>X</button>
+							<p className="Title">2018-2019</p>
+							<p>Safety standards such as OSHA and ASME were adhered to with the regards to pod design, testing, and operation whenever relevant. Furthermore, the year’s propulsion system was modified to be cold gas thrusters which was primarily chosen to reduce the pod’s high voltage requirements and increase electrical safety.</p>
+						</div>
+						 : null }
   					</div>
 
   					
@@ -84,5 +104,6 @@ const About_body = () =>{
 		</React.Fragment>
 	);
 }
+
 
 export default About_body;
