7d83a4befe22bbfad4a89a6be8911975711714b2
nivrithikrishnan <41163965+nivrithikrishnan@users.noreply.github.com>
2021-05-19 20:56:11 -0700

    added flexbox for poddetails

--- src/components/podDetails/podDetails_body.js
+++ src/components/podDetails/podDetails_body.js
@@ -6,24 +6,32 @@ import React, { useEffect } from 'react';
 const PodDetails_body = () =>{
 	return(
 		<React.Fragment>
-			<div className="oneloop-poddetails-title"> 
-				<h1> Pod Details </h1> 
-			</div> 
-
-			<div className="oneloop-poddetails-subtitle"> 
-				<h2> Photo Title </h2>
+			<div className="oneloop-poddetails-heading"> 
+				<div className="oneloop-poddetails-title"> 
+					<h1> Pod Details </h1> 
+				</div> 
+				<div className="oneloop-poddetails-mainphoto"> 
+				</div>
 			</div>
 
-			<div className="oneloop-poddetails-caption">
-				<h3>Caption here</h3>
+			<div className="oneloop-poddetails-section1"> 
+				<div className="oneloop-poddetails-subtitle"> 
+					<h2> Photo Title </h2>
+				</div>
+
+				<div className="oneloop-poddetails-caption">
+					<h3>Caption here</h3>
+				</div>
 			</div>
 
-			<div className="oneloop-poddetails-subtitle"> 
-				<h2> Photo Title </h2>
-			</div>
+			<div className="oneloop-poddetails-section2"> 
+				<div className="oneloop-poddetails-subtitle"> 
+					<h2> Photo Title </h2>
+				</div>
 
-			<div className="oneloop-poddetails-caption">
-				<h3>Caption here</h3>
+				<div className="oneloop-poddetails-caption">
+					<h3>Caption here</h3>
+				</div>
 			</div>
 
 		</React.Fragment>
