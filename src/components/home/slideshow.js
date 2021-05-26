1bff9e88c264b79395303b3b9d3bba3f64567d2f
sarahayu <46186848+sarahayu@users.noreply.github.com>
2021-05-25 20:38:28 -0700

    change slideshow divs to img and fix font sizes

--- src/components/home/slideshow.js
+++ src/components/home/slideshow.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
+// preload images so page doesn't jitter when slideshow changes images
+// might remove this once we actually have images?
+function Preloaded(props) {
+    return (
+        <div style={{ display: 'none' }}>
+            { props.images.map((img, i) => (<img key={ i } src={ img } alt=""/>)) }
+        </div>
+    );
+}
+
+function Dots(props) {
+    return (
+        <React.Fragment>
+            {
+                props.images.map((_, offset) => (
+                    <span key={ offset } className={ offset === props.imageOffset ? 'active' : '' } onClick={ () => props.setImageOffset(offset) }>
+                    </span>
+                ))
+            }
+        </React.Fragment>
+    )
+}
+
 function Slideshow() {
 
     // changes images once they're available
     const Images = [
-        'https://placekitten.com/200/400',
-        'https://placekitten.com/505/400',
-        'https://placekitten.com/510/400',
-        'https://placekitten.com/520/400'
+        'https://placekitten.com/500/200',
+        'https://placekitten.com/500/201',
+        'https://placekitten.com/500/202',
+        'https://placekitten.com/500/203',
+        'https://placekitten.com/500/204',
+        'https://placekitten.com/500/205',
+        'https://placekitten.com/500/206'
     ];
 
     const [ imageOffset, setImageOffset ] = useState(0);
-
-    const Dots = Images.map((image, offset) => (
-        <span key={ offset } className={ offset === imageOffset ? 'active' : '' } onClick={ () => setImageOffset(offset) }>
-        </span>
-    ));
 
     useEffect(() => {
         const timer = setTimeout(() => setImageOffset((imageOffset + 1) % Images.length), 3000);
@@ -28,14 +49,15 @@ function Slideshow() {
     return (
         <div className="slideshow">
             <div className="frame-wrapper">                
-                <div key={ imageOffset } 
+                <img key={ imageOffset } 
                     className="ss-image"
-                    style={{ backgroundImage: `url(${Images[imageOffset]})` }}></div>
-                <div key={ (imageOffset - 1 + Images.length) % Images.length } 
+                    src={ Images[imageOffset] } />
+                <img key={ (imageOffset - 1 + Images.length) % Images.length } 
                     className="ss-image ss-idle" 
-                    style={{ backgroundImage: `url(${Images[(imageOffset - 1 + Images.length) % Images.length]})` }}></div>
+                    src={ Images[(imageOffset - 1 + Images.length) % Images.length]} />
             </div>
-            { Dots }
+            <Dots imageOffset={ imageOffset } setImageOffset={ setImageOffset } images={ Images } />
+            <Preloaded images={ Images } />
         </div>
     );
 }
