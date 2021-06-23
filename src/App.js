70174948041d0ca9523b5b282f4ccfde6340c219
Alec Atienza <alec@null.net>
2021-06-22 19:13:41 -0700

    fixed spacing

--- src/App.js
+++ src/App.js
@@ -32,8 +32,7 @@ function App() {
 						<About />
 					</Route>
 					<Route path="/joinus">
-						<GoogleReCaptchaProvider reCaptchaKey="6Lea004bAAAAADrzGSu1G0j94611UG7DXo3SPI_2
-">
+						<GoogleReCaptchaProvider reCaptchaKey="6Lea004bAAAAADrzGSu1G0j94611UG7DXo3SPI_2">
 							<Join />
 						</GoogleReCaptchaProvider>
 					</Route>
