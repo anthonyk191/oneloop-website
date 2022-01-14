a4e172c5b37c8eefbfa8fb13bfd7709e4dc337d6
Shyam Agarwal <shyagarwal@ucdavis.edu>
2022-01-13 17:48:17 -0800

    Update team2021_body.js

--- src/components/team/team2021_body.js
+++ src/components/team/team2021_body.js
@@ -316,7 +316,7 @@ const Team2020_body = () =>{
 				<div className="oneloop-team2019 stability team-web">
 					<div className="stability-row row row-1">
 						<div className="team-member">
-							<img className="team-member" src="/images/team/20212022/Forrest_Fankhauser.jpeg"/>
+							<img className="team-member" src="/images/team/20212022/Forrest_Fankhauser.jpg"/>
 							<h4>FORREST FANKHAUSER<br/>
 							<span>Power Co-Lead</span></h4>
 						</div>
